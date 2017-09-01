1c9f9eac9c54368950879de2837ecc31996f3b7f
Sharad Bhat <sharad.mbhat@gmail.com>
2017-09-02 03:42:58 +0530

    Add files via upload

---
+++ TableToMySQLStatement.py
@@ -0,0 +1,46 @@
+import csv
+import sys
+import os.path
+
+file_name = ""
+
+try:
+    file_name = sys.argv[1]
+except:
+    print("No filename provided")
+
+if os.path.isfile(file_name) == False:
+    print("No such file present in current directory")
+else:
+    table_name = ""
+    try:
+        table_name = sys.argv[2]
+    except:
+        print("No table name provided")
+
+    csvFile = open(file_name, "rt", encoding="utf-8")
+    csvReader = csv.reader(csvFile)
+    csvFile.seek(0)
+
+    MySQL_statement = "INSERT INTO {} VALUES".format(table_name)
+    line_number = 1
+    for row in csvReader:
+        item_number = 1
+        if line_number == 1:
+            MySQL_statement += "("
+            line_number += 1
+        else:
+            MySQL_statement += ",("
+        for item in row:
+            if item_number != 1:
+                MySQL_statement += ", "
+            if item.isnumeric():
+                MySQL_statement += item
+                item_number += 1
+            else:
+                MySQL_statement += "\"{}\"".format(item)
+                item_number += 1
+
+        MySQL_statement += ")"
+    MySQL_statement += ";"
+    print(MySQL_statement)
