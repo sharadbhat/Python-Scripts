abcf6f0a0130b27623890000a2f1116e628310ed
Sharad Bhat <sharad.mbhat@gmail.com>
2017-11-09 01:20:10 +0530

    Added argparse

--- MySQL to CSV/sql_to_csv.py
+++ MySQL to CSV/sql_to_csv.py
@@ -1,19 +1,27 @@
-import os
 import csv
 import pymysql
+import argparse
+import os
 
-host = "HOST_NAME" #localhost
-user = "MySQL_USERNAME" # root
-password = "MySQL_PASSWORD"
-database_name = "DATABASE_NAME"
+parser = argparse.ArgumentParser(description='Convert SQL database to CSV files.')
+parser.add_argument("-ht", "--host", help="Enter MySQL host name")
+parser.add_argument("-u", "--user", help="Enter MySQL username")
+parser.add_argument("-p", "--password", help="Enter MySQL password")
+parser.add_argument("-db", "--database", help="Enter database name")
+args = parser.parse_args()
+
+host = args.host # localhost
+user = args.user # root
+password = args.password
+database_name = args.database
 
 db = pymysql.connect(host=host, user=user, passwd=password, db=database_name)
 cur = db.cursor()
 
+cur.execute("SHOW TABLES")
+
 os.mkdir("./{}".format(database_name))
 os.chdir("./{}".format(database_name))
-
-cur.execute("SHOW TABLES")
 
 for row_1 in cur.fetchall():
     table_name = row_1[0]
