f8e76f0edaf4b6efc9f9792dc3b949d0cc0fa451
Sharad Bhat <sharad.mbhat@gmail.com>
2017-11-28 02:48:09 +0530

    Added getting random image url

--- Random XKCD/random_xkcd.py
+++ Random XKCD/random_xkcd.py
@@ -1,1 +1,17 @@
+import requests
+import bs4
 
+url = "https://c.xkcd.com/random/comic/"
+
+r = requests.get(url)
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+count = 0
+for img in soup.find_all('img'):
+    image_url = "http:" + str(img.get('src'))
+    image_title = img.get('title')
+    if count == 1:
+        break
+    count = 1
+
+print(image_url)
+print(image_title)
