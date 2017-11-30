60ec448015bc678f67aaf36faa705d5fbf790013
Sharad Bhat <sharad.mbhat@gmail.com>
2017-11-30 23:42:29 +0530

    Added StackOverflowReader

---
+++ StackOverflowReader/stack_overflow_reader.py
@@ -0,0 +1,10 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Get answers from Stack Overflow')
+parser.add_argument("-q", "--query", help="Enter question to search SO")
+parser.add_argument("-t", "--tag", help="Enter question tag")
+args = parser.parse_args()
+
+query = args.query
+tag = args.tag
