ab68c454f2b960f315e185859389ce60046bbd38
Sharad Bhat <sharad.mbhat@gmail.com>
2018-02-13 22:53:42 +0530

    Added instance checker

---
+++ Instance Checker/instance_checker.py
@@ -0,0 +1,15 @@
+import sys
+import psutil
+import os
+import time
+
+file_name = sys.argv[0]
+
+for pid in psutil.pids():
+    p = psutil.Process(pid)
+    if os.getpid() != pid:
+        if p.name() == "python.exe" and len(p.cmdline()) > 1 and file_name in p.cmdline()[1]:
+            print("Instance already running")
+            sys.exit(0)
+
+time.sleep(100)
